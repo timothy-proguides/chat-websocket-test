93d438b7b733aec0de79af4274c895d5dfb6495f
sadisticsolution <sadisticsolutione@gmail.com>
2019-07-10 18:51:20 -0700

    Added universal route to log everything

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,13 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::any('{uri}', function (\Illuminate\Http\Request $request) {
+    $message = "";
+    $message .= "Method: " . $request->method() . "\n";
+    $message .= "Uri: " . $request->fullUrl() . "\n";
+    $message .= "Data: " . json_encode($request->all()) . "\n";
+    $message .= "\n";
+
+    Log::info($message);
+    return "HERE";
+})->where('uri', '.*');
